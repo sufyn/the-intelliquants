8a1ddcb21de7c7cf8150d3eaaac881e0c1f60c6d
Yahya.AbdulSattar <106555655+yahyaAbdulSattar@users.noreply.github.com>
2023-05-10 00:54:46 +0530

    Add files via upload
    Quiz app without flash feature

---
+++ Quiz page/card.js
@@ -0,0 +1,116 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }]
+
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+            { text: "Mars", correct: false },
+            { text: "Jupiter", correct: true },
+            { text: "Venus", correct: false },
+            { text: "Saturn", correct: false }]
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        answers: [
+            { text: "Mount Everest", correct: true },
+            { text: "K2", correct: false },
+            { text: "Makalu", correct: false },
+            { text: "Cho Oyu", correct: false }]
+
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHtml = "Next";
+    showQuestion();
+}
+
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}.`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
